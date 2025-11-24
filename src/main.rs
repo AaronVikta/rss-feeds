c9213b20e8dbc4c196a617faf2c1634069acb12e
Aaron Victor <aeinsteinvikta@gmail.com>
2025-11-24 14:36:15 +0100

    CLI commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+
+
+#[derive(Parser)]
+#[command(name = "rss_cli")]
+struct Cli {
+    //The RSS feed URL
+    #[command(subcommand)]
+    command: Actions,
+}
+
+
+#[derive(Debug,Subcommand)]
+enum Actions{
+
+    Add{
+    #[arg(short, long)]
+    url: String
+    },
+
+    //Fetch 5 RSS feeds from the given URL
+    Fetch{
+    #[arg(short, long)]
+    url: String,
+    },
+    Read{
+    #[arg(short, long)]
+    id: usize
+    }
+}
+
 fn main() {
     println!("A CLI based RSS FEED");
 }
+
